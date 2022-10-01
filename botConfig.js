5aed6aff2615888089388d77d5d483521ec3b19e
SKGAMIN <114792749+SKGAMIN@users.noreply.github.com>
2022-10-01 10:27:39 +0530

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "MTAyNTQ0MDEzNTY2MTI0MDQ2MA.GJXN9e.9zpcL0zReMdro6ZENKu2c0oEYiZnwFv_MPi7Rs",
+    prefix: "SK",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "877212239805956136"
 ],
     debug: true,
-    countChannel: "countChannelID"
+    countChannel: "1025629728847167529"
 };
